5bd41cd6e54fa985b07ecf0864e16780ec2625b8
Rishitha <trishithapandu2218@gmail.com>
2024-12-14 19:27:42 +0530

    Add files via upload

---
+++ outputformatting.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("================================");
+        for (int i = 0; i < 3; i++) {
+            String str = sc.next(); 
+            int num = sc.nextInt(); 
+            System.out.printf("%-15s%03d%n", str, num);
+        }
+        System.out.println("================================");
+        sc.close();
+    }
+}
+
